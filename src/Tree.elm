1075a5cde4120fb2ea8c78f6ac2d0bb2fc410b55
Rupert Smith <rupert@thesett.com>
2018-06-26 17:06:23 +0100

    Shaping up the tea-tree API.

--- src/Tree.elm
+++ src/Tree.elm
@@ -1,103 +1,155 @@
-module Tree exposing (..)
+module Tree
+    exposing
+        ( Tree
+        , Zipper
+        , Path
+          -- Tree operations
+        , empty
+        , zipper
+        , map
+          -- Zipper operations
+        , goToChild
+        , goToRightMostChild
+        , goUp
+        , goLeft
+        , goRight
+        , goToRoot
+        , goToNext
+        , goToPrevious
+        , goTo
+        , updateFocusDatum
+        , datum
+        , insertChild
+        , updateChildren
+        , getPath
+          -- Path operations
+        , goToPath
+        , updateDatum
+        )
 
 -- It will be a multiway Tree implementation, not a binary tree.
 --
 
 
 type Path
-    = List Int
+    = Path Id (List Int)
 
 
-type Tree a
-    = Tree a (Forest a)
+type alias Id =
+    Int
 
 
-type alias Forest a =
-    List (Tree a)
+type Tree
+    = Tree
+        { nextId : Id
+        , nodeData : NodeArray a
+        , innerTree : InnerTree
+        }
 
 
-type Context a
-    = Context a (Forest a) (Forest a)
+type InnerTree
+    = InnerTree
+        { id : Id
+        , children : Forest
+        }
 
 
-type alias Breadcrumbs a =
-    List (Context a)
+type alias Forest =
+    List InnerTree
 
 
-type alias Zipper a =
-    ( Tree a, Breadcrumbs a )
+type Context
+    = Context
+        { id : Id -- Needed here?
+        , focus : Focus -- At top-level in zipper or here?
+        , pre : Forest
+        , post : Forest
+        }
+
+
+type alias Breadcrumbs =
+    List Context
+
+
+type alias NodeArray a =
+    Array Int a
 
 
 type alias Focus =
-    { currentId : Int
+    { nextId : Id
+    , currentPath : Path
     }
 
 
+type Zipper a
+    = Zipper
+        { nextId : Id -- In the focus or here?
+        , nodeData : NodeArray a
+        , innerTree : InnerTree
+        , crumbs : Breadcrumbs
+        }
 
--- Every node will be marked with a unique id, so that re-walking the tree from a Path
--- can be confirmed as correct. Walking a Path will produce a Maybe.
 
 
+-- Tree operations
+--, empty
+--, zipper
+--, map
+--
+-- Zipper operations
+-- , goToChild
+-- , goToRightMostChild
+-- , goUp
+-- , goLeft
+-- , goRight
+
+
+{-| The Tree will only be able to be manipulated through TreeExplorer or Zipper.
+The Tree implementation data structure will not be exposed; it will be opaque.
+This is so that all work on the Tree can carry around a Focus context;
+in particular, a current id stamp used to label all nodes with unique references.
+Ataching and removing this Focus context will happen within the implementation
+and not something that the caller has to remember and pass around.
+
+Walking the zipper context back to the root will produce a Tree and a Path.
+If just the Path is needed, that can be extracted efficiently, without walking
+back to the root, by the `getPath` function.
+
+-}
+goToRoot : Zipper a -> ( Tree a, Path )
+
+
+
+-- , goToNext
+-- , goToPrevious
+-- , goTo
+-- , updateFocusDatum
+-- , datum
+-- , insertChild
+-- , updateChildren
+-- , getPath
+--
+-- Path operations
+
+
+{-| The Path and Tree can be recombined to recover a previous position in the tree.
 walkPath : Path -> Tree a -> Maybe (Zipper a)
 
+Every node will be marked with a unique id, so that re-walking the tree from a Path
+can be confirmed as correct. Walking a Path will produce a Maybe.
+
+This allows events to be tagged with Paths which describe a return to a
+previously visited position within a tree, without capturing any other data
+associated with that node. This is to circumvent the stale data issue when
+a user is interacting with a tree.
+
+-}
+goToPath : Path -> Tree a -> Maybe (Zipper a)
 
 
---
--- The Tree will only be able to be manipulated through TreeExplorer or Zipper.
--- The Tree implementation data structure will not be exposed; it will be opaque.
--- This is so that all work on the Tree can carry around a Focus context;
--- in particular, a current id stamp used to label all nodes with unique references.
--- Ataching and removing this Focus context will happen within the implementation
--- and not something that the caller has to remember and pass around.
---
--- Walking the zipper context back to the root will produce a Tree and a Path.
-
-
-walkToRoot : Zipper a -> ( Tree a, Path )
-
-
-
---
--- The Path and Tree can be recombined to recover a previous position in the tree.
--- walkPath : Path -> Tree a -> Maybe (Zipper a)
---
--- This allows events to be tagged with Paths which describe a return to a
--- previously visited position within a tree, without capturing any other data
--- associated with that node. This is to circumvent the stale data issue when
--- a user is interacting with a tree.
---
--- The contents of nodes in the tree will be held in an `Array Id a`. Ids will be assigned
--- sequentially. This will allow mapping by id without re-walking a Path possible. It will
--- only be necessary to re-walk paths when adding new nodes into the tree, as this is the only
--- situation when fresh ids will need to be generated.
-
-
-mapNode : Path -> (a -> a) -> Tree a -> Tree a
-
-
-
---
--- This more complex scheme may allow a more efficient return to a single position
--- within a Tree than using `indexedMap`. This should be tested; for example could
--- a tree with hundreds of visible nodes have many of them animated in parallel
--- or would there be too much garbage collection churn when running this data structure?
--- type alias Path =
---     List Int
---
---
--- type Tree a
---     = Tree a (Forest a)
---
---
--- type alias Forest a =
---     List (Tree a)
---
---
--- type TreeExplorer a
---     = Branch (Forest a) TreeExplorer (Forest a)
---     | Focus Whatever Tree Tree
---
---
--- type alias Focus =
---     { currentId : Int
---     }
+{-| The contents of nodes in the tree will be held in an `Array Id a`. Ids will be assigned
+sequentially. This will allow mapping by id without re-walking a Path possible. It will
+only be necessary to re-walk paths when adding new nodes into the tree, as this is the only
+situation when fresh ids will need to be generated.
+-}
+updateDatum : Path -> (a -> a) -> Tree a -> Tree a
